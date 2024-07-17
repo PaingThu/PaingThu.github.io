ebb2ebedb83697d157004b44bbcdd9fed7544a77
Maw Paing Thu <paingmawthu@gmail.com>
2024-07-17 23:59:49 +0900

    Modified for SEO - title, description, keywords

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,18 @@
+import { describe } from "node:test";
+
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
     compatibilityDate: '2024-04-03',
     devtools: { enabled: true },
+    app:{
+        head:{
+            title: "MAW PAING THU",
+            meta:[
+                { name: "description", content: "Full-Stack Developer Vue.js & Nuxt.js & Tailwind & SASS/SCSS & PHP" },
+                { name: "keywords", content: "MAW PAING THU, Frontend Developer, Full-Stack Developer, PHP API Developer, Vue Developer, Nuxt Developer"  }
+            ]
+        }
+    },
     css: ['~/assets/scss/main.scss'],
     postcss: {
         plugins: {
