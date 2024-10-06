ca71280df8e35fcb5ab9996e9d301a4cd6cdbb77
Maw Paing Thu <paingmawthu@gmail.com>
2024-10-06 22:55:50 +0900

    Added Google Tag Manager

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -16,7 +16,12 @@ export default defineNuxtConfig({
                     name: "keywords", 
                     content: "MAW PAING THU, Frontend Developer, Full-Stack Developer, PHP API Developer, Vue Developer, Nuxt Developer"  
                 }
-            ]
+            ],
+            script:[{innerHTML: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-5JHCZZRP');`}]
         }
     },
     css: ['~/assets/scss/main.scss'],
