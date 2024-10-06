f7cec305af3dd22ee40a3f040b3f9a946a9a03d2
Maw Paing Thu <paingmawthu@gmail.com>
2024-10-06 23:20:59 +0900

    Modify Google Analytics

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -17,11 +17,6 @@ export default defineNuxtConfig({
                     content: "MAW PAING THU, Frontend Developer, Full-Stack Developer, PHP API Developer, Vue Developer, Nuxt Developer"  
                 }
             ],
-            script:[{innerHTML: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-})(window,document,'script','dataLayer','GTM-5JHCZZRP');`}]
         }
     },
     css: ['~/assets/scss/main.scss'],
@@ -31,5 +26,9 @@ j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
         autoprefixer: {},
         },
     },
-    modules: ['@vueuse/nuxt', "@nuxt/image"]
+    modules: ['@vueuse/nuxt', "@nuxt/image", 'nuxt-gtag'],
+
+    gtag: {
+        id: 'GTM-5JHCZZRP'
+    }
 })
